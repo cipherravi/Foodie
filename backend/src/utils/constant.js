af23c607d2a023e58bea8211792b99245bb83109
Ravi Yadav <148179385+cipherravi@users.noreply.github.com>
2025-05-05 09:49:26 +0530

    changed origins in cors

--- backend/src/utils/constant.js
+++ backend/src/utils/constant.js
@@ -1,6 +1,4 @@
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:3000",
   "https://cipherravi-foodie.vercel.app", // 👈 your production URL
   "https://keep-alive-rbb2.onrender.com",
 ];
