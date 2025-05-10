4ad70b162a4cc4d09157b8803aa3e7a878b18ef3
Ravi Yadav <148179385+cipherravi@users.noreply.github.com>
2025-05-10 23:09:10 +0530

    add local ip

--- backend/src/utils/constant.js
+++ backend/src/utils/constant.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-  "http://localhost",
+  "http://localhost:5173",
   "https://cipherravi-foodie.vercel.app", // 👈 your production URL
   "https://keep-alive-rbb2.onrender.com",
-  "http://192.168.196.224",
+  "http://192.168.196.224:5173",
 ];
 module.exports = { allowedOrigins };
