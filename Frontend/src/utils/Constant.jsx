c968b0a1690e6422e03bdd993ae46558dd44e365
Ravi Yadav <148179385+cipherravi@users.noreply.github.com>
2025-05-11 17:41:58 +0530

    fix url

--- Frontend/src/utils/Constant.jsx
+++ Frontend/src/utils/Constant.jsx
@@ -1,4 +1,4 @@
 export const url = "https://media-assets.swiggy.com/swiggy/image/upload/";
 
-export const RESTAURANTS_URL = "http://localhost:3000/api/v1/restaurants";
-// "https://foodie-backend-so1x.onrender.com/api/v1/restaurants";
+export const RESTAURANTS_URL =
+  "https://foodie-backend-so1x.onrender.com/api/v1/restaurants";
