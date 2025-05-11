fc0ed2adfb9297d383d89f19f3da7d74af775b80
Ravi Yadav <148179385+cipherravi@users.noreply.github.com>
2025-05-11 09:13:12 +0530

    made changes in login&signup

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
   return (
     <div>
       <div className="wrapper w-screen h-[90vh] flex justify-center items-center">
-        <div className="box flex flex-col justify-start items-center w-[70%] h-1/2 sm:w-[45%] md:w-[40%] lg:w-[30%] xl:w-[28%]   bg-[#fde6e6]  p-8 rounded-lg shadow-lg gap-5">
+        <div className="box flex flex-col justify-start items-center w-[70%] h-[55%] sm:w-[45%] md:w-[40%] lg:w-[30%] xl:w-[28%]   bg-[#fde6e6]  p-8 rounded-lg shadow-lg gap-5">
           <h1 className="form-heading  sm:text-3xl font-gilroy-bold text-2xl">
             Login
           </h1>
@@ -38,9 +38,9 @@ function Login() {
           />
           <button
             type="submit"
-            className="submitw-[55%] h-[11%] sm:w-[45%] md:w-[40%] lg:w-[34%] xl:w-[30%]  text-sm font-gilroy-medium bg-[#b80000] cursor-pointer text-white rounded-md border-none"
+            className="submit w-[50%] h-[10%] py-3 sm:w-[45%] md:w-[40%] lg:w-[34%] xl:w-[30%]  text-sm font-gilroy-medium bg-[#b80000] cursor-pointer text-white rounded-md border-none flex items-center justify-center"
           >
-            Login
+            <p>Login</p>
           </button>
           <span className="term-condition  w-full   px-0 font-gilroy-medium text-xs  text-center ">
             By clicking on Login, I accept the{" "}
@@ -51,7 +51,7 @@ function Login() {
           <span className="create-account font-gilroy-medium text-sm">
             <span>or </span>
             <Link to="/signup">
-              <span className="select-none font-gilroy-bold cursor-pointer">
+              <span className="select-none font-gilroy-bold cursor-pointer ">
                 create an account
               </span>
             </Link>
--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -14,7 +14,7 @@ const SignUp = () => {
   return (
     <div>
       <div className="wrapper  w-screen h-[90vh]  flex justify-center items-center">
-        <div className="box  flex flex-col justify-start items-center w-[70%]  h-1/2  sm:w-[45%] md:w-[40%] lg:w-[30%] xl:w-[28%] bg-[#fde6e6]  p-8 rounded-lg shadow-lg gap-5">
+        <div className="box  flex flex-col justify-start items-center w-[70%]  h-[55%]  sm:w-[45%] md:w-[40%] lg:w-[30%] xl:w-[28%] bg-[#fde6e6]  p-8 rounded-lg shadow-lg gap-5">
           <h1 className="form-heading sm:text-3xl lg:text-4xl font-gilroy-bold text-2xl">
             Sign Up
           </h1>
@@ -38,9 +38,9 @@ const SignUp = () => {
           />
           <button
             type="submit"
-            className="submit   w-[55%] h-[13%] sm:w-[52%] sm:h-[10%] md:w-[55%] lg:w-[56%] xl:w-[50%] text-sm font-gilroy-medium bg-[#b80000] cursor-pointer text-white rounded-md border-none"
+            className="submit  p-2 w-[55%] h-[15%] sm:w-[52%] sm:h-[13%] md:w-[55%] lg:w-[56%] xl:w-[50%] text-xs font-gilroy-medium bg-[#b80000] cursor-pointer text-white rounded-md border-none flex items-center justify-center"
           >
-            Create an account
+            <p> Create an account </p>
           </button>
           <span className="term-condition  w-full  px-0  font-gilroy-medium text-xs  text-center ">
             By clicking on Login, I accept the{" "}
